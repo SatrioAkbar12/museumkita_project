e2161028c6a20577d4cc48f9825790c29d84c69d
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-06-23 15:46:48 +0700

    update data museum

--- lib/data/data_museum.dart
+++ lib/data/data_museum.dart
@@ -7,7 +7,7 @@ List<MuseumModel> getMuseum() {
   museumModel.nama = "Museum Bank Rakyat Indonesia";
   museumModel.kota = "Kabupaten Banyumas";
   museumModel.provinsi = "Jawa Tengah";
-  museumModel.imgHeader = "museum-bri";
+  museumModel.imgHeader = "museum-bri.jpg";
   museumModel.deskripsi = "Museum BRI merupakan bekas gedung lama dan pertama Bank BRI. Museum ini menyimpan banyak sekali koleksi barang dan peralatan yang dulunya dipakai untuk operasional Bank BRI seperti mesin pembukuan dari berbagai merk, pesawat telepon, peti besi, lemari besi, mesin penghitung uang dan lainnya. Ada juga koleksi uang kertas dan koin lama dari berbagai negara. ";
   museumModel.alamat = "Jl. Jend. Soedirman No.57, Pesayangan, Kedungwuluh, Purwokerto Bar., Kabupaten Banyumas, Jawa Tengah";
   museumModel.htm = "5000";
@@ -21,7 +21,7 @@ List<MuseumModel> getMuseum() {
   museumModel.nama = "Museum BPK";
   museumModel.kota = "Magelang";
   museumModel.provinsi = "Jawa Tengah";
-  museumModel.imgHeader = "museum-bpk";
+  museumModel.imgHeader = "museum-bpk.jpg";
   museumModel.deskripsi = "Badan Pemeriksa Keuangan (BPK) membangun sebuah museum yang mencatat sejarah perjalanan lembaga tinggi negara tersebut agar lebih dikenal oleh masyarakat luas. Museum BPK merupakan gedung tua dengan nuansa arsitektur Belanda tersebut pada mulanya merupakan salah satu bagian dari kantor Karesidenan Kedu. Museum ini memiliki koleksi berupa benda hiraldika, buku, grafika, reflika, koleksi hasil abstraksi, etnografika, dan miniatur.";
   museumModel.alamat = "Jl. Pangeran Diponegoro No.1, Magelang, Kec. Magelang Tengah, Kota Magelang, Jawa Tengah";
   museumModel.htm = "Gratis";
@@ -35,7 +35,7 @@ List<MuseumModel> getMuseum() {
   museumModel.nama = "Museum Batik Kota Pekalongan";
   museumModel.kota = "Pekalongan";
   museumModel.provinsi = "Jawa Tengah";
-  museumModel.imgHeader = "museum-batik-kota-pekalongan";
+  museumModel.imgHeader = "museum-batik-kota-pekalongan.jpg";
   museumModel.deskripsi = "Museum Batik Pekalongan memiliki luas tanah dan bangunan sebesar 40 meter persegi dan memiliki 1149 koleksi batik, antara lain wayang beber dari kain batik yang berusia ratusan tahun dan alat tenun tradisional atau dikenal sebagai alat tenun bukan mesin.";
   museumModel.alamat = "Jl. Jatayu No.3, Panjang Wetan, Pekalongan Utara, Kota Pekalongan, Jawa Tengah";
   museumModel.htm = "2000-10000";
@@ -43,6 +43,34 @@ List<MuseumModel> getMuseum() {
   museumModel.noTelepon = "(0285) 431698";
   museumModel.email = "museum.batik@yahoo.com";
   museumModel.website = "https://museumbatikpekalongan.info/";
+  museum.add(museumModel);
+  museumModel = new MuseumModel();
+
+  museumModel.nama = "Museum Isdiman";
+  museumModel.kota = "Semarang";
+  museumModel.provinsi = "Jawa Tengah";
+  museumModel.imgHeader = "museum-isdiman.jpg";
+  museumModel.deskripsi = "Museum Isdiman memiliki koleksi lengkap pakaian TKR dan tentara Belanda, lukisan teknik supit urang, senjata-senjata, dan  juga memajang beberapa foto wajah hitam-putih beberapa orang tentara Indonesia yang memiliki peran penting dalam pertempuran Ambarawa. Selain itu juga terdapat Monumen Palagan Ambarawa yang didirikan untuk mengenang pertempuran Palagan di Ambarawa di masa awal kemerdekaan Republik Indonesia.";
+  museumModel.alamat = "Jl. Mgr. Sugiyopranoto, Panjang Lor, Ambarawa, Semarang, Jawa Tengah";
+  museumModel.htm = "7500";
+  museumModel.persyaratan = [];
+  museumModel.noTelepon = "(024) 6921424";
+  museumModel.email = "";
+  museumModel.website = "";
+  museum.add(museumModel);
+  museumModel = new MuseumModel();
+
+  museumModel.nama = "Museum Istana Mangkunegaran";
+  museumModel.kota = "Surakarta";
+  museumModel.provinsi = "Jawa Tengah";
+  museumModel.imgHeader = "museum-istana-magkunegaran.jpg";
+  museumModel.deskripsi = "Museum Puro Mangkunegaran berada di dalam kompleks istana Mangkunegaran. Museum ini menyimpan koleksi benda-benda bersejarah milik Puro Mangkunegaran yang dikumpulkan sejak tahun 1926. Museum Puro Mangkunegaran dibuka untuk umum pada tahun 1968 dan dikelola oleh Pariwisata Mangkunegaran.";
+  museumModel.alamat = "Jl. Ronggowarsito No.83, Keprabon, Kec. Banjarsari, Kota Surakarta, Jawa Tengah";
+  museumModel.htm = "10000-20000";
+  museumModel.persyaratan = [];
+  museumModel.noTelepon = "0271-642016";
+  museumModel.email = "puromangkunegaran@gmail.com";
+  museumModel.website = "https://puromangkunegaran.com/museum/";
   museum.add(museumModel);
   museumModel = new MuseumModel();
 
@@ -57,6 +85,8 @@ List<MuseumModel> getMuseum() {
   // museumModel.noTelepon = "";
   // museumModel.email = "";
   // museumModel.website = "";
+  // museum.add(museumModel);
+  // museumModel = new MuseumModel();
 
   return museum;
 }
