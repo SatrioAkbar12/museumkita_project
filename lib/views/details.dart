c0ee6daac7f3ed4d2760c3717a969db3ee233fac
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2021-06-23 22:27:10 +0700

    update details.dart

--- lib/views/details.dart
+++ lib/views/details.dart
@@ -26,8 +26,16 @@ class Details extends StatefulWidget {
 }
 
 class _DetailsState extends State<Details> {
+  // final int indeks;
+
+  // _DetailsState({
+  //   @required this.indeks
+  // });
+  
   List<CountryModel> country = new List();
   List<MuseumModel> museum = [];
+
+  // final int indeks = this.indeks;
 
   @override
   void initState() {
@@ -49,7 +57,7 @@ class _DetailsState extends State<Details> {
               Stack(
                 children: [
                   Image(
-                    image: AssetImage('assets/image-museum/'+museum[indeks].imgHeader),
+                    image: AssetImage('assets/image-museum/'+museum[widget.indeks].imgHeader),
                     height: 350,
                     width: MediaQuery.of(context).size.width,
                     fit: BoxFit.cover,
@@ -108,7 +116,7 @@ class _DetailsState extends State<Details> {
                             children: [
                               Text(
                                 // widget.placeName,
-                                museum[indeks].nama,
+                                museum[widget.indeks].nama,
                                 style: TextStyle(
                                     color: Colors.white,
                                     fontWeight: FontWeight.w600,
@@ -129,7 +137,7 @@ class _DetailsState extends State<Details> {
                                   ),
                                   Text(
                                     // "Koh Chang Tai, Thailand",
-                                    museum[indeks].kota+", "+museum[indeks].provinsi,
+                                    museum[widget.indeks].kota+", "+museum[widget.indeks].provinsi,
                                     style: TextStyle(
                                         color: Colors.white70,
                                         fontWeight: FontWeight.w500,
@@ -211,7 +219,7 @@ class _DetailsState extends State<Details> {
                 padding: EdgeInsets.symmetric(horizontal: 24),
                 child: Text(
                   // "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut scelerisque arcu quis eros auctor, eu dapibus urna congue. Nunc nisi diam, semper maximus risus dignissim, semper maximus nibh. Sed finibus ipsum eu erat finibus efficitur. ",
-                  museum[indeks].deskripsi,
+                  museum[widget.indeks].deskripsi,
                   textAlign: TextAlign.start,
                   style: TextStyle(
                       fontSize: 15,
@@ -223,6 +231,36 @@ class _DetailsState extends State<Details> {
               SizedBox(
                 height: 16,
               ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                child: Row(
+                  children: [
+                    Text(
+                      // "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut scelerisque arcu quis eros auctor, eu dapibus urna congue. Nunc nisi diam, semper maximus risus dignissim, semper maximus nibh. Sed finibus ipsum eu erat finibus efficitur. ",
+                      "Alamat : ",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          fontSize: 15,
+                          height: 1.5,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xff879D95)),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width*0.75,
+                      child: Text(
+                        // "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut scelerisque arcu quis eros auctor, eu dapibus urna congue. Nunc nisi diam, semper maximus risus dignissim, semper maximus nibh. Sed finibus ipsum eu erat finibus efficitur. ",
+                        museum[widget.indeks].alamat,
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                            fontSize: 15,
+                            height: 1.5,
+                            fontWeight: FontWeight.w600,
+                            color: Color(0xff879D95)),
+                      ),
+                    ),
+                  ],
+                ),
+              )
               // Container(
               //   height: 240,
               //   child: ListView.builder(
